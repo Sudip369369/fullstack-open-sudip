d7ff42bada2d4ea4499a035e825d9fc6d3b7b0c7
SUDIP GC <sudeeep369@gmail.com>
2025-08-21 11:03:29 +0330

    Added exercise of part 4

---
+++ part4/blog-app/controllers/users.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body
+
+    if (!password || password.length < 3) {
+      return response.status(400).json({
+        error: 'password missing or too short (minimum length is 3 characters)'
+      })
+    }
+
+    if (!username || username.length < 3) {
+      return response.status(400).json({
+        error: 'username missing or too short (minimum length is 3 characters)'
+      })
+    }
+
+    // Check if username already exists
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      return response.status(400).json({
+        error: 'expected `username` to be unique'
+      })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = usersRouter
